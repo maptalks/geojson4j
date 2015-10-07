494589181506e747e2e0884497dbca0dca4ef6b7
fuzhen <fuzhen@maptalks.org>
2015-10-07 20:26:35 +0800

    add CRS create methods

--- src/main/java/org/maptalks/gis/core/geojson/CRS.java
+++ src/main/java/org/maptalks/gis/core/geojson/CRS.java
@@ -26,8 +26,63 @@ public class CRS {
         this.properties = properties;
     }
 
+    /**
+     * create a Chinese Coordinate Type CRS.
+     * example:
+     * "crs": {"type": "cnCoordinateType","properties": {"name": "gcj02"}}
+     * possible values: gcj02, cgcs2000, bd09ll, wgs84
+     * @param coordinateType
+     * @return
+     */
+    public static CRS createCnCoordinateType(final String coordinateType) {
+        CRS crs = new CRS();
+        crs.setType("cnCoordinateType");
+        crs.setProperties(new HashMap<String, Object>(){
+            {
+                put("name", coordinateType);
+            }
+        });
+        return crs;
+    }
 
+    /**
+     * create a standard CRS.
+     * this is recommended by geojson.org
+     * @link http://geojson.org/geojson-spec.html#named-crs
+     * example:
+     * "crs": {"type": "name","properties": {"name": "urn:ogc:def:crs:OGC:1.3:CRS84"}}
+     * @param name
+     * @return
+     */
+    public static CRS createCRS(final String name) {
+        CRS crs = new CRS();
+        crs.setType("name");
+        crs.setProperties(new HashMap<String, Object>(){
+            {
+                put("name", name);
+            }
+        });
+        return crs;
+    }
 
+    /**
+     * create a EPSG style CRS recommended by spatialreference.org.
+     * @link http://spatialreference.org/ref/epsg/4326/json/
+     * example:
+     * {'type': 'EPSG', 'properties': {'code': 4326}}
+     * @param code
+     * @return
+     */
+    public static CRS createEPSGCRS(final String code) {
+        CRS crs = new CRS();
+        crs.setType("EPSG");
+        crs.setProperties(new HashMap<String, Object>(){
+            {
+                put("code", code);
+            }
+        });
+        return crs;
+    }
 
 
 }
