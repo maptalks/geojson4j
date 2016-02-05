7f4c423a4d7fb3b7821a4039b6a1e1a8891e568a
fuzhen <fuzhen@maptalks.org>
2016-02-05 10:41:23 +0800

    move ProjectionTransform to maptalks-geojson

---
+++ src/main/java/org/maptalks/gis/core/geojson/common/ProjectionTransform.java
@@ -0,0 +1,132 @@
+package org.maptalks.gis.core.geojson.common;
+
+import org.maptalks.gis.core.geojson.*;
+import org.maptalks.gis.core.geojson.ext.Circle;
+import org.maptalks.gis.core.geojson.ext.Ellipse;
+import org.maptalks.gis.core.geojson.ext.Rectangle;
+import org.maptalks.gis.core.geojson.ext.Sector;
+import org.maptalks.proj4.Proj4;
+import org.maptalks.proj4.Proj4Exception;
+
+public class ProjectionTransform {
+
+    public static Geometry transform(Geometry geometry, CRS f, CRS t) throws Proj4Exception {
+        if (f == null || t == null) {
+            return geometry;
+        }
+        if (f.equals(t)) {
+            return geometry;
+        }
+
+        // XXX: currently only support proj4 style crs
+        if (!f.getType().equals("proj4") || !t.getType().equals("proj4")) {
+            return geometry;
+        }
+
+        String fp = (String) f.getProperties().get("proj");
+        String tp = (String) t.getProperties().get("proj");
+        Proj4 proj4 = new Proj4(fp, tp);
+
+        return transform(geometry, proj4);
+    }
+
+    public static Geometry transform(Geometry geometry, Proj4 proj4) throws Proj4Exception {
+        if (geometry instanceof Point) {
+            double[] c = convert(((Point) geometry).getCoordinates(), proj4);
+            return new Point(c);
+        } else if (geometry instanceof LineString) {
+            double[][] c = convert(((LineString) geometry).getCoordinates(), proj4);
+            return new LineString(c);
+        } else if (geometry instanceof Polygon) {
+            double[][][] c = convert(((Polygon) geometry).getCoordinates(), proj4);
+            return new Polygon(c);
+        } else if (geometry instanceof MultiPoint) {
+            double[][] c = convert(((MultiPoint) geometry).getCoordinates(), proj4);
+            return new MultiPoint(c);
+        } else if (geometry instanceof MultiLineString) {
+            double[][][] c = convert(((MultiLineString) geometry).getCoordinates(), proj4);
+            return new MultiLineString(c);
+        } else if (geometry instanceof MultiPolygon) {
+            double[][][][] c = convert(((MultiPolygon) geometry).getCoordinates(), proj4);
+            return new MultiPolygon(c);
+        } else if (geometry instanceof GeometryCollection) {
+            Geometry[] geometries = ((GeometryCollection) geometry).getGeometries();
+            if (geometries != null) {
+                int size = geometries.length;
+                Geometry[] array = new Geometry[size];
+                for (int i = 0; i < size; i++) {
+                    array[i] = transform(geometries[i], proj4);
+                }
+                return new GeometryCollection(array);
+            }
+            return new GeometryCollection();
+        } else if (geometry instanceof Sector) {
+            Sector sector = ((Sector) geometry);
+            double[] c = convert(sector.getCoordinates(), proj4);
+            return new Sector(c, sector.getRadius(), sector.getStartAngle(), sector.getEndAngle(), sector.getUnit());
+        } else if (geometry instanceof Circle) {
+            Circle circle = ((Circle) geometry);
+            double[] c = convert(circle.getCoordinates(), proj4);
+            return new Circle(c, circle.getRadius(), circle.getUnit());
+        } else if (geometry instanceof Ellipse) {
+            Ellipse ellipse = ((Ellipse) geometry);
+            double[] c = convert(ellipse.getCoordinates(), proj4);
+            return new Ellipse(c, ellipse.getWidth(), ellipse.getHeight(), ellipse.getUnit());
+        } else if (geometry instanceof Rectangle) {
+            Rectangle rect = ((Rectangle) geometry);
+            double[] c = convert(rect.getCoordinates(), proj4);
+            return new Rectangle(c, rect.getWidth(), rect.getHeight(), rect.getUnit());
+        }
+        return null;
+    }
+
+    public static double[][][][] convert(double[][][][] coordinates, Proj4 proj4) throws Proj4Exception {
+        if (coordinates == null) {
+            return null;
+        }
+        if (coordinates.length == 0) {
+            return new double[0][0][0][0];
+        }
+        double[][][][] result = new double[coordinates.length][][][];
+        for (int i = 0; i < coordinates.length; i++) {
+            result[i] = convert(coordinates[i], proj4);
+        }
+        return result;
+    }
+
+    public static double[][][] convert(double[][][] coordinates, Proj4 proj4) throws Proj4Exception {
+        if (coordinates == null) {
+            return null;
+        }
+        if (coordinates.length == 0) {
+            return new double[0][0][0];
+        }
+        double[][][] result = new double[coordinates.length][][];
+        for (int i = 0; i < coordinates.length; i++) {
+            result[i] = convert(coordinates[i], proj4);
+        }
+        return result;
+    }
+
+    public static double[][] convert(double[][] coordinates, Proj4 proj4) throws Proj4Exception {
+        if (coordinates == null || coordinates.length == 0) {
+            return null;
+        }
+        double[][] result = new double[coordinates.length][];
+        for (int i = 0, len = coordinates.length; i < len; i++) {
+            double[] coordinate = coordinates[i];
+            double[] converted = convert(coordinate, proj4);
+            if (converted != null) {
+                result[i] = converted;
+            } else {
+                result[i] = coordinates[i];
+            }
+        }
+        return result;
+    }
+
+    public static double[] convert(double[] coordinate, Proj4 proj4) throws Proj4Exception {
+        return proj4.forward(coordinate);
+    }
+
+}
